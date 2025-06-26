2218667158062a81c1c60f4b8c415f730929ff55
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-06-26 14:20:48 +1000

    Added new elmetn for suspense

--- src/app/(public)/(full-width-pages)/(auth)/verify/page.tsx
+++ src/app/(public)/(full-width-pages)/(auth)/verify/page.tsx
@@ -1,43 +1,14 @@
 'use client'
 
-import { useEffect, useState } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
+import React, { Suspense } from 'react';
+import VerifyContent from '@/app/lib/helpers/verifyPage/verifypage';
 
 export const dynamic = 'force-dynamic';
 
-
 export default function VerifyPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const token = searchParams.get('token');
-  const [status, setStatus] = useState('Verifying...');
-
-  useEffect(() => {
-    if (!token) {
-      setStatus('Missing token');
-      return;
-    }
-
-    const verifyToken = async () => {
-      const res = await fetch(`/api/verify?token=${token}`);
-      const data = await res.json();
-
-      if (res.ok) {
-        setStatus('Verification successful! Redirecting to sign in...');
-        setTimeout(() => {
-          router.push(`/signup?verified=${token}`);
-        }, 2000);
-      } else {
-        setStatus(data.error || 'Verification failed');
-      }
-    };
-
-    verifyToken();
-  }, [token]);
-
   return (
-    <div className="p-6 text-center">
-      <h1 className="text-xl font-semibold">{status}</h1>
-    </div>
+    <Suspense fallback={<div>Loading verification...</div>}>
+      <VerifyContent />
+    </Suspense>
   );
 }
---
+++ src/app/lib/helpers/verifyPage/verifypage.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+
+export default function VerifyContent() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const token = searchParams.get('token');
+  const [status, setStatus] = useState('Verifying...');
+
+  useEffect(() => {
+    if (!token) {
+      setStatus('Missing token');
+      return;
+    }
+
+    const verifyToken = async () => {
+      const res = await fetch(`/api/verify?token=${token}`);
+      const data = await res.json();
+
+      if (res.ok) {
+        setStatus('Verification successful! Redirecting to sign in...');
+        setTimeout(() => {
+          router.push(`/signup?verified=${token}`);
+        }, 2000);
+      } else {
+        setStatus(data.error || 'Verification failed');
+      }
+    };
+
+    verifyToken();
+  }, [token]);
+
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-xl font-semibold">{status}</h1>
+    </div>
+  );
+}
