3be79233510c92547e89fc3a583d6186a0166500
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-06-26 14:42:06 +1000

    Added new middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const authCookie = request.cookies.get("auth");
+
+  // Step 1: If already authenticated, skip auth
+  if (authCookie?.value === "1") {
+    return NextResponse.next();
+  }
+
   const basicAuth = request.headers.get("authorization");
 
   if (basicAuth) {
     const authValue = basicAuth.split(" ")[1];
     const [user, pwd] = atob(authValue).split(":");
 
-    if (user === process.env.NEXT_PUBLIC_BASIC_AUTH_USER &&
-      pwd === process.env.NEXT_PUBLIC_BASIC_AUTH_PASS) {
-      return NextResponse.next()
+    if (
+      user === process.env.NEXT_PUBLIC_BASIC_AUTH_USER &&
+      pwd === process.env.NEXT_PUBLIC_BASIC_AUTH_PASS
+    ) {
+      const res = NextResponse.next();
+      res.cookies.set("auth", "1", {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24, // 1 day
+        path: "/",
+      });
+      return res;
     }
-  
   }
 
+  // Step 3: Not authenticated
   return new Response("Auth required", {
     status: 401,
     headers: {
